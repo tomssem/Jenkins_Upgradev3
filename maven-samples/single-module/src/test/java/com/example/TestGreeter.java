6e3700d0ed295f039313bd1729bd6917bab94d82
Tom Nicholson <tfwnicholson+1@gmail.com>
2020-09-22 12:13:28 +0900

    Add a new dummy test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void greetShouldIncludeGreetingLongMessage() {
+    String someone = "World, Thanks you making me really Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
